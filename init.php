03a94151a6fcfc6cdfb738b7b2fa1b252f1f422d
EdHallProper <ed@properdesign.co.uk>
2023-09-28 14:58:10 +0100

    Add author name and website

--- init.php
+++ init.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name:  ACF custom map field plugin
-Plugin URI:   https://www.wpbeginner.com
+Plugin URI:  https://properdesign.co.uk/
 Description:  A short little description of the plugin. It will be displayed on the Plugins page in WordPress admin area.
 Version:      1.0
-Author:       WPBeginner
-Author URI:   https://www.wpbeginner.com
+Author:       Proper design
+Author URI:  https://properdesign.co.uk/
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  proper-icf
